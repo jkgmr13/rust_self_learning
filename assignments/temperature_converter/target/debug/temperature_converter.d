b59d69eaaecc2f5cb26de36932d6524a7de31118
jkgmr13 <84328587+jkgmr13@users.noreply.github.com>
2022-10-04 20:56:48 -0600

    Delete assignments/temperature_converter/target directory
    removing stuff that should have been in git ignore

--- assignments/temperature_converter/target/debug/temperature_converter.d
+++
@@ -1,1 +0,0 @@
-/home/joeknit/Desktop/rust_self_practice/assignments/temperature_converter/target/debug/temperature_converter: /home/joeknit/Desktop/rust_self_practice/assignments/temperature_converter/src/main.rs
